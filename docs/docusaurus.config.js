7c7d983c961168e9105967f6869e563b9221142e
janeshin059 <janeshin059@gmail.com>
2020-11-29 23:40:35 -0500

    Diagram

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -84,7 +84,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/romanenko/covid19-safety-information',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
